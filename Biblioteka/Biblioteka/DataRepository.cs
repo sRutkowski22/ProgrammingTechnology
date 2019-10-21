773a8c3f537b58d09ff3f49f80119159c3f7c427
Szymon <s.rutkowski235@gmail.com>
2019-10-21 17:02:15 +0200

    Część 3 bez testów i bez metod update

--- Biblioteka/Biblioteka/DataRepository.cs
+++ Biblioteka/Biblioteka/DataRepository.cs
@@ -13,6 +13,57 @@ namespace Biblioteka
             this.fill = fill;
             wypelnienie.fillIn(dataContext);
         }
-      
+      //C.R.U.D ADD GET ALL GET UPDATE DELETE
+      void AddKatalog(Katalog k, DataContext data)
+        {
+            data.dictionaryKatalog.Add((data.dictionaryKatalog.Count + 1),k);
+        }
+      void GetKatalog(Katalog k, DataContext data)
+        {
+            for(int i=0; i<data.dictionaryKatalog.Count; i++)
+            {
+                if (data.dictionaryKatalog.ContainsValue(k))
+                {
+                    //TODO: do zrobienia wypisywanie
+                    Console.WriteLine(data.dictionaryKatalog);
+                }
+            }
+        }
+        void GetAllKatalog(DataContext data)
+        {
+            for (int i = 0; i < data.dictionaryKatalog.Count; i++)
+            {
+                Console.WriteLine(data.dictionaryKatalog.ToString());
+            }
+        }
+        void UpdateKatalog(DataContext data, Katalog k)
+        {
+            
+        }
+        void DeleteKatalog(DataContext data, int key)
+        {
+            data.dictionaryKatalog.Remove(key);
+        }
+        void AddWykaz(DataContext data, Wykaz w)
+        {
+            data.WykazList.Add(w);
+        }
+        void GetAll(DataContext data)
+        {
+            foreach(Wykaz w in data.WykazList)
+            {
+                w.ToString();
+            }
+        }
+        Wykaz GetWykaz(DataContext data, Wykaz w)
+        {
+            for(int i=0; i < data.WykazList.Count;i++)
+            {
+                if (data.WykazList.Contains(w))
+                {
+                    return data.WykazList[i];
+                }
+            }
+        }
     }
 }
