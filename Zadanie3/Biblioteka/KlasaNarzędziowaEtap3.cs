554f0222e69bd2851223a5f07d05763727997be9
Przemek <216827@edu.p.lodz.pl>
2019-12-11 16:35:33 +0100

    etap 3 tests

--- Zadanie3/Biblioteka/KlasaNarzędziowaEtap3.cs
+++ Zadanie3/Biblioteka/KlasaNarzędziowaEtap3.cs
@@ -10,94 +10,94 @@ namespace Biblioteka
         public static List<Product> GetProductsByName(string name)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
-            List<Product> zapytanie =
+            List<Product> products =
                 (from p in db.Products
                  where SqlMethods.Like(p.Name, "%" + name + "%")
                  select p).ToList();
 
-            return zapytanie;
+            return products;
         }
 
         public static List<Product> GetProductsByVendorName(string vendorName)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
 
-            List<Product> zapytanie = (from p in db.Products
+            List<Product> products = (from p in db.Products
                                        join pv in db.ProductVendors on p.ProductID equals pv.ProductID
                                        join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
                                        where SqlMethods.Like(v.Name, vendorName)
                                        select p).ToList();
-            return zapytanie;
+            return products;
         }
 
         public static List<string> GetProductNamesByVendorName(string vendorName)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
 
-            List<string> zapytanie = (from p in db.Products
+            List<string> productNames = (from p in db.Products
                                       join pv in db.ProductVendors on p.ProductID equals pv.ProductID
                                       join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
                                       where SqlMethods.Like(v.Name, vendorName)
                                       select p.Name).ToList();
 
-            return zapytanie;
+            return productNames;
         }
         public static string GetProductVendorByProductName(string productName)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
 
-            var zapytanie = (from p in db.Products
+            var productVendor = (from p in db.Products
                              join pv in db.ProductVendors on p.ProductID equals pv.ProductID
                              join v in db.Vendors on pv.BusinessEntityID equals v.BusinessEntityID
                              where SqlMethods.Like(p.Name, productName)
                              select v.Name).FirstOrDefault();
 
-            return zapytanie.ToString();
+            return productVendor.ToString();
         }
 
         public static List<Product> GetProductsWithNRecentReviews(int howManyReviews)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
 
-            List<Product> zapytanie = (from pr in db.ProductReviews
+            List<Product> products = (from pr in db.ProductReviews
                                        join p in db.Products on pr.ProductID equals p.ProductID
                                        select p).Take(howManyReviews).ToList<Product>();
 
-            return zapytanie;
+            return products;
         }
         public static List<Product> GetNRecentlyReviewedProducts(int howManyProducts)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
 
-            List<Product> zapytanie = (from p in db.Products
+            List<Product> products = (from p in db.Products
                                        join pr in db.ProductReviews on p.ProductID equals pr.ProductID
                                        orderby pr.ReviewDate
                                        select p).Take(howManyProducts).ToList<Product>();
 
-            return zapytanie;
+            return products;
         }
         public static List<Product> GetNProductsFromCategory(string categoryName, int n)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
 
-            List<Product> zapytanie = (from p in db.Products
+            List<Product> products = (from p in db.Products
                                        join s in db.ProductSubcategories on p.ProductSubcategoryID equals s.ProductSubcategoryID
                                        join c in db.ProductCategories on s.ProductCategoryID equals c.ProductCategoryID
                                        where c.Name == categoryName
                                        select p).Take(n).ToList();
 
-            return zapytanie;
+            return products;
         }
         public static double GetTotalStandardCostByCategory(ProductCategory category)
         {
             DataClasses1DataContext db = new DataClasses1DataContext();
 
-            var zapytanie = (from p in db.Products
+            var totalStandardCost = (from p in db.Products
                              join s in db.ProductSubcategories on p.ProductSubcategoryID equals s.ProductSubcategoryID
                              join c in db.ProductCategories on s.ProductCategoryID equals c.ProductCategoryID
                              where c.Equals(category)
                              select p);
-            return zapytanie.Sum(p => System.Convert.ToDouble(p.ListPrice));
+            return totalStandardCost.Sum(p => System.Convert.ToDouble(p.ListPrice));
         }
 
 
--- Zadanie3/Test/KlasaNarzędziowaEtap3Tests.cs
+++ Zadanie3/Test/KlasaNarzędziowaEtap3Tests.cs
@@ -10,16 +10,105 @@ namespace Test
     [TestClass]
     public class KlasaNarzędziowaEtap3Tests
     {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
         [TestMethod()]
         public void GetProductsByNameTest()
         {
-            DataClasses1DataContext db = new DataClasses1DataContext();
-            List<Product> zapytanie = KlasaNarzędziowaEtap3.GetProductsByName("Decal");
+           
+            List<Product> products = KlasaNarzędziowaEtap3.GetProductsByName("Blade");
 
             List<Product> stala = new List<Product>();
-            Product C = (from p in db.Products where p.ProductID == 325 select p).First();
+            Product C = (from p in db.Products where p.ProductID == 316 select p).First();
             stala.Add(C);
-            C = (from p in db.Products where p.ProductID == 326 select p).First();
+        
+
+            for (int i = 0; i < products.Count(); i++)
+            {
+                if (products[i].ProductID != stala[i].ProductID) Assert.Fail();
+                if (products[i].Name != stala[i].Name) Assert.Fail();
+            }
+            if (products.Count() != stala.Count()) Assert.Fail();
+        }
+        [TestMethod()]
+        public void GetProductsByVendorNameTest()
+        {
+        
+
+            List<Product> products = KlasaNarzędziowaEtap3.GetProductsByVendorName("Allenson Cycles");
+
+            List<Product> stala = new List<Product>();
+            Product C = (from p in db.Products where p.ProductID == 530 select p).First();
+            stala.Add(C);
+
+            if (products[0].ProductID != stala[0].ProductID) Assert.Fail();
+            if (products[0].Name != stala[0].Name) Assert.Fail();
+
+        }
+
+        [TestMethod()]
+        public void GetProductNamesByVendorNameTest()
+        {
+         
+
+            List<string> products = KlasaNarzędziowaEtap3.GetProductNamesByVendorName("Allenson Cycles");
+
+            List<Product> stala = new List<Product>();
+            Product C = (from p in db.Products where p.ProductID == 530 select p).First();
+            stala.Add(C);
+           if (products[0] != stala[0].Name) Assert.Fail();
+           
+
+        }
+
+        [TestMethod()]
+        public void GetProductVendorByProductNameTest()
+        {
+         
+
+            string zapytanie = KlasaNarzędziowaEtap3.GetProductVendorByProductName("Decal 1");
+
+            string str = "SUPERSALES INC.";
+            if (str != zapytanie) Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetProductsWithNRecentReviewsTest()
+        {
+          
+
+            List<Product> zapytanie = KlasaNarzędziowaEtap3.GetProductsWithNRecentReviews(10);
+
+            List<Product> stala = new List<Product>();
+            Product C = (from p in db.Products where p.ProductID == 709 select p).First();
+            stala.Add(C);
+            C = (from p in db.Products where p.ProductID == 937 select p).First();
+            stala.Add(C);
+            stala.Add(C);
+            C = (from p in db.Products where p.ProductID == 798 select p).First();
+            stala.Add(C);
+
+            for (int i = 0; i < zapytanie.Count(); i++)
+            {
+                if (zapytanie[i].ProductID != stala[i].ProductID) Assert.Fail();
+                if (zapytanie[i].Name != stala[i].Name) Assert.Fail();
+            }
+            if (zapytanie.Count() != stala.Count()) Assert.Fail();
+        }
+        [TestMethod()]
+        public void GetNRecentlyReviewedProductsTest()
+        {
+         
+
+            List<Product> zapytanie = KlasaNarzędziowaEtap3.GetNRecentlyReviewedProducts(10);
+
+            List<Product> stala = new List<Product>();
+            Product C = (from p in db.Products where p.ProductID == 709 select p).First();
+            stala.Add(C);
+            C = (from p in db.Products where p.ProductID == 937 select p).First();
+            stala.Add(C);
+            stala.Add(C);
+            C = (from p in db.Products where p.ProductID == 798 select p).First();
             stala.Add(C);
 
             for (int i = 0; i < zapytanie.Count(); i++)
@@ -30,6 +119,42 @@ namespace Test
             if (zapytanie.Count() != stala.Count()) Assert.Fail();
         }
 
-      
+        [TestMethod()]
+        public void GetNProductsFromCategoryTest()
+        {
+           
+
+            List<Product> zapytanie = KlasaNarzędziowaEtap3.GetNProductsFromCategory("Bikes", 2);
+
+            List<Product> stala = new List<Product>();
+            Product C = (from p in db.Products where p.ProductID == 771 select p).First();
+            stala.Add(C);
+            C = (from p in db.Products where p.ProductID == 772 select p).First();
+            stala.Add(C);
+
+            for (int i = 0; i < zapytanie.Count(); i++)
+            {
+                if (zapytanie[i].ProductID != stala[i].ProductID) Assert.Fail();
+                if (zapytanie[i].Name != stala[i].Name) Assert.Fail();
+            }
+            if (zapytanie.Count() != stala.Count()) Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTotalStandardCostByCategoryTest()
+        {
+           
+            ProductCategory C = (from p in db.ProductCategories where p.ProductCategoryID == 1 select p).First();
+
+            decimal zapytanie = (decimal) KlasaNarzędziowaEtap3.GetTotalStandardCostByCategory(C);
+            decimal final = (decimal)153913.49;
+            //Console.WriteLine(zapytanie);
+            //Console.WriteLine(153913.49);
+
+            if (final != zapytanie) Assert.Fail();
+            //if(Tools.GetTotalStandardCostByCategory(C) != 153913.49) Assert.Fail();
+        }
+
+
     }
 }
