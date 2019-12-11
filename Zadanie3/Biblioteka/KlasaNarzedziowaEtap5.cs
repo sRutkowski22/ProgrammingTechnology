cf25608091b663bd8b8273503191e33bc3c40b05
Przemek <216827@edu.p.lodz.pl>
2019-12-11 21:36:28 +0100

    etap 5 testy

--- Zadanie3/Biblioteka/KlasaNarzedziowaEtap5.cs
+++ Zadanie3/Biblioteka/KlasaNarzedziowaEtap5.cs
@@ -7,24 +7,16 @@ using System.Threading.Tasks;
 
 namespace Biblioteka
 {
-    class KlasaNarzedziowaEtap5
+    public class KlasaNarzedziowaEtap5
     {
         public static List<MyProduct> GetProductsByVendorName(string vendorName)
         {
             MyProductContext data = new MyProductContext();
-            data.Products.Where(p => p.ProductVendors.Where(pv => pv.Vendor.Name.Contains(vendorName));
-           // data.Products.Select(p=> new String()
-           // return (from product in data.Products
-          //          join pv in )
-           
+
+            return data.Products.Where(p => p.ProductVendors.Any(pv => pv.Vendor.Name == vendorName)).ToList();
+            
         }
-        public static List<MyProduct> GetNMyProductsFromCategory(string categoryName, int n)
-        {
-            MyProductContext data = new MyProductContext();
-            return (from product in data.Products
-                    where product.ProductSubcategory != null && product.ProductSubcategory.ProductCategory.Name.Equals(categoryName)
-                    select product).Take(n).ToList();
-        }
+    
         public static List<MyProduct> GetMyProductsByName(string namePart)
         {
             MyProductContext data = new MyProductContext();
---
+++ Zadanie3/Test/KlasaNarzędziowaEtap5Testscs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Biblioteka;
+using System.Collections.Generic;
+
+namespace Test
+{
+    [TestClass]
+    public class KlasaNarzędziowaEtap5Testscs
+    {
+        [TestClass]
+        public class MyProductTest
+        {
+
+            [TestMethod]
+            public void GetProductsByVendorName()
+            {
+                List<MyProduct> products = KlasaNarzedziowaEtap5.GetProductsByVendorName("Allenson Cycles");
+
+                if (products[0].ProductID != 530) Assert.Fail();
+           
+            }
+
+
+            [TestMethod]
+            public void GetMyProductsByName()
+            {
+                int ilosc = KlasaNarzedziowaEtap5.GetMyProductsByName("Blade").Count;
+                Assert.AreEqual(1, ilosc);
+            }
+
+          
+
+            [TestMethod]
+            public void GetMyProductsWithNRecentReviews()
+            {
+                List<MyProduct> products = KlasaNarzedziowaEtap5.GetMyProductsWithNRecentReviews(1);
+                Assert.AreEqual(products.Count, 2);
+                Assert.IsNotNull(products.Find(product => product.ProductID == 709));
+                Assert.IsNotNull(products.Find(product => product.ProductID == 798));
+            }
+        }
+    }
+}
+
