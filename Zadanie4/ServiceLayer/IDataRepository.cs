9a675d5f2cd0fde33d1c1cc7b7b2b5702d02686e
Przemek <216827@edu.p.lodz.pl>
2020-01-20 20:59:56 +0100

    add dataLayer and start working on ServiceLayer

---
+++ Zadanie4/ServiceLayer/IDataRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    interface IDataRepository
+    {
+        //C.R.U.D ADD GET ALL GET UPDATE DELETE
+
+        // ADD
+        void AddLocation();
+        // GET ALL
+        IQueryable<Location> GetAllLocations();
+        // GET 
+        Location GetLocation(int id);
+        // UPDATE
+        void UpdateLocation(int id);
+        // DELETE
+        void DeleteLocation(int id);
+     
+    }
+}
